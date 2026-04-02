f5f050247d5f3c32e2c6654b80ca7dbacfed8d0f
tzwchen <tristanandkauai@icloud.com>
2026-04-01 22:57:13 -0700

    reimplemented isnan, working on fill_random_array

--- isnan.asm
+++ isnan.asm
@@ -8,47 +8,13 @@ segment .bss
 
 segment .text
 
+isnan:
+    ; argument is passed in xmm0 for x86-64 calling conventions
+    ucomisd xmm0, xmm0   ; compare value to itself
+    jp .is_nan           ; unordered means NaN
+    mov eax, 1           ; false: not NaN
+    ret
 
-;Block that backs up almost all GPRs
-;Back up the general purpose registers for the sole purpose of protecting the data of the caller.
-push rbp                                        
-mov  rbp,rsp                                    
-push rdi                                          
-push rsi                                          
-push rdx                                          
-push rcx                                         
-push r8                                           
-push r9                                           
-push r10                                          
-push r11                                          
-push xmm0                                          
-push xmm1                                                
-push rbx                                          
-pushf                                            
-
-;pass value to xmm0
-mov xmm0, rdi
-
-check:
-ucomisd xmm0, xmm0 ;comp with each other
-jp .is_nan ;jump if parity
-mov rax, 0 ;return false
-
-is_nan:
-mov rax, -1 ;return true
-
-popf                                    
-pop rbx                                 
-pop xmm1
-pop xmm0
-pop r11                                 
-pop r10                                 
-pop r9                                 
-pop r8                                  
-pop rcx                                 
-pop rdx                                 
-pop rsi                                 
-pop rdi                                 
-pop rbp                                 
-
-
+.is_nan:
+    xor eax, eax         ; true: is NaN
+    ret
